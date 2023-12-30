15ff6ebf87dfb297fa7324123ca7781cd4f3ca39
SvabhuG <svabhug@gmail.com>
2023-12-30 18:23:35 -0500

    handling multiple clients

--- backend.py
+++ backend.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
+import random
+import string
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+custom_id_to_sid = {}
 
 @app.route('/')
 def index():
@@ -10,8 +13,11 @@ def index():
 
 @socketio.on('message')
 def handle_message(msg):
-    print('Received message: ', msg)
-    socketio.emit('message', msg)
+    socketio.emit('message', 'Client ' + str(session_id) + ": " + msg)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+    
+def generate_custom_id():
+    # Generate a random 6-character alphanumeric custom ID
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
